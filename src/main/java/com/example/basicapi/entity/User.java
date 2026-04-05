98d36f87eaf2e038406e69a7ec3ab280fcccc3d4
enzoleao <enzopinheiro6@gmail.com>
2026-04-05 16:03:11 -0300

    feat: criando entidade usuarios

---
+++ src/main/java/com/example/basicapi/entity/User.java
@@ -0,0 +1,20 @@
+package com.example.basicapi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
